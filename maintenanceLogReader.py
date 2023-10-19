1b1189f9acad64dd2660df9596298b46ddfb22df
Seiya Nozawa-Temchenko <94246020+seiyant@users.noreply.github.com>
2023-10-19 08:22:02 -0700

    Update maintenanceLogReader.py

--- maintenanceLogReader.py
+++ maintenanceLogReader.py
@@ -69,12 +69,6 @@ def extract_data_from_docx(file_path):
         if row.cells[2].text != "A":
             crew_details.append((name, position, aid))
 
-# Define function to read PDF documents
-def extract_data_from_pdf(file_path):
-    # Load pdf file
-    pdf = PdfReader(file_path)
-
-# Define function to read PDF documents
 # Define function to fetch Maximo status using Selenium
 # Define function to write to Excel
 # Main function or script execution
