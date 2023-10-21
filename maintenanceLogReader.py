815024b0e12e71fc12c9dac1a85e4a19cd20602f
seiyant <seiyant@student.ubc.ca>
2023-10-20 21:27:24 -0700

    Added Excel reading

--- maintenanceLogReader.py
+++ maintenanceLogReader.py
@@ -12,9 +12,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from PyPDF2 import PdfReader
 from docx import Document
-import datetime as time
+import datetime
 import xlwings as Excel
 
 # Ensure browser version and web driver version match
@@ -47,27 +46,53 @@ Cont2Elem.click()
 iframe = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[7]/iframe")))
 browser.switch_to.frame(iframe)
 
-# Define function to read Word documents
-crew_details = []
-
+# Extract the work details from the Word document
 def extract_data_from_docx(file_path):
     # Load docx file
     docx = Document(file_path)
 
     # Extract the date and format it
-    date_cell = docx.cell(1, 1).text
-    date_string = date_cell.split()
-    date_object = time.strptime(date_string, '%b %d, %Y')
-    format_date = date_object.strptime('%m/%d/%Y')
+    date_cell = docx.tables[0].cell(1, 1).text
+    date_object = datetime.datetime.strptime(date_cell, '%b %d, %Y')
+    formatted_date = date_object.strftime('%m/%d/%Y')  
 
-    # Extract the crew list
-    for row in docx.rows[9:]:
-        name = row.cells[0].text
-        position = row.cells[1].text
-        aid = row.cells[3].text
+    # Initialize an empty list to store the work details
+    work_details = []
 
-        if row.cells[2].text != "A":
-            crew_details.append((name, position, aid))
+    # Search for the row that contains "JOBS ASSIGNED TO" in column 0
+    start_row_index = None
+    for index, row in enumerate(docx.tables[0].rows):
+        if row.cells[0].text.lower() == "JOB ASSIGNED TO":
+            start_row_index = index
+            break
+    
+    # If "JOB ASSIGNED TO" is found, extract subsequent rows
+    if start_row_index is not None:
+        for row in docx.tables[0].rows[start_row_index + 1:]:
+            job_assigned_to = row.cells[0].text
+            description = row.cells[1].text
+            status = row.cells[2].text
+            work_details.append((job_assigned_to, description, status))
+
+    return formatted_date, work_details
+
+# Cross-reference with Excel
+def get_full_name(excel_path):
+    # Open Excel workbook and select sheet
+    wb = Excel.Book(excel_path)
+    sheet = wb.sheets[0]
+
+    # Get full name (column 1) and initials (column 4)
+    full_name = sheet.range('B1:B' + str(sheet.cells.last_cell.row)).value
+    initials = sheet.range('E1:E' + str(sheet.cells.last_cell.row)).value
+    
+    # Create a disctionary to map initials to full names
+    initials_dictionary = {initial: name for initial, name in zip(initials, full_names)}
+
+    return initials_dictionary
+
+def name_to_code(work_details, sheet, crew):
+    # Dictionary 
 
 # Define function to fetch Maximo status using Selenium
 # Define function to write to Excel
