a740b6001c7537bb3d1b6de8adc4e3a600cd4d8f
Seiya Nozawa-Temchenko <seiyant01@gmail.com>
2024-02-20 14:47:39 -0800

    feb 20

--- biweeklyHoursPlanner.py
+++ biweeklyHoursPlanner.py
@@ -78,7 +78,7 @@ more_search.click()
 # Set Types to HKG, INR, or PPM
 types = wait.until(EC.element_to_be_clickable((By.ID, 'med325893-tb')))
 types.click()
-types.send_keys('=HKG,=INR,=PPM')
+types.send_keys('=HKG,=INR,=PPM') #ishmam had this idk why its set to these and not all
 
 # Garbage value to ensure things load
 garbage_value = wait.until(EC.element_to_be_clickable((By.ID, 'm8db33e5c-tb')))
@@ -116,109 +116,78 @@ else:
 print(f'Total Work Orders: {numberofWOs}')
 
 time.sleep(2)
-wo1Elem = browser.find_element(By.ID, "m6a7dfd2f_tdrow_[C:1]_ttxt-lb[R:0]") #change ID here if diff
-type(wo1Elem) #wo1 = work order one
-time.sleep(1)
+wo1Elem = wait.until(EC.element_to_be_clickable((By.ID, "m6a7dfd2f_tdrow_[C:1]_ttxt-lb[R:0]"))) 
 wo1Elem.click() #click first work order
 time.sleep(1)
-i=0
-while i<numberofWOs:
-    time.sleep(4)
-    wb.save() #save in excel
+
+i = 0
+while i < numberofWOs:
+    # Navigate to Work Order
+    if i == 0:
+        wo_html = f'm6a7dfd2f_tdrow_[C:1]_ttxt-lb[R:{i}]'
+        findwo = wait.until(EC.element_to_be_clickable((By.ID, wo_html)))
+        findwo.click()
+    else:
+        try:
+            findwo = wait.until(EC.element_to_be_clickable((By.ID, 'toolactions_NEXT-tbb_anchor')))
+            findwo.click()
+            time.sleep(2)
+            nav2wo = wait.until(EC.element_to_be_clickable((By.ID,'mbf28cd64-tab_anchor')))
+            nav2wo.click()
+
+        except: #system message
+            syserr = wait.until(EC.element_to_be_clickable((By.ID, 'm96ad0396-pb')))
+            syserr.click()
+            time.sleep(2)
+            findwo = wait.until(EC.element_to_be_clickable((By.ID, 'toolactions_NEXT-tbb_anchor')))
+            findwo.click()
+            time.sleep(2)
+            nav2wo = wait.until(EC.element_to_be_clickable((By.ID,'mbf28cd64-tab_anchor')))
+            nav2wo.click()
+
+    # Navigate in Work Order
     time.sleep(2)
-    
-    WO = browser.find_element(By.XPATH, "/html/body/form/div/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/div/table/tbody/tr[3]/td/table/tbody/tr/td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr[2]/td/input").get_attribute("value");
-    description = browser.find_element(By.XPATH, "/html/body/form/div/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/div/table/tbody/tr[3]/td/table/tbody/tr/td[1]/div/table/tbody/tr[1]/td/div/table/tbody/tr/td/table/tbody/tr[2]/td/input").get_attribute("value");
-    jobplan = browser.find_element(By.XPATH, "/html/body/form/div/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr/td[1]/div/table/tbody/tr/td/div/table/tbody/tr[2]/td/table/tbody/tr[2]/td/input").get_attribute("value");
-    tstart = browser.find_element(By.XPATH, "/html/body/form/div/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[5]/td/div/table/tbody/tr/td/table/tbody/tr/td[1]/div/table/tbody/tr/td/div/table/tbody/tr[2]/td/table/tbody/tr/td[1]/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr[2]/td/input").get_attribute("value");
-    asset = browser.find_element(By.XPATH, "/html/body/form/div/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/div/table/tbody/tr[3]/td/table/tbody/tr/td[1]/div/table/tbody/tr[1]/td/div/table/tbody/tr/td/table/tbody/tr[10]/td/input[1]").get_attribute("value");
-    PlansElem = browser.find_element(By.ID, "m356798d1-tab_anchor") #all of the above are text boxes, the .get_attribute() automatically takes the value in a single line. The left is a tab button
-    time.sleep(3)
-    type(PlansElem)
-    PlansElem.click()#click plans tab
-    time.sleep(5) 
-    try:
-        plannedhrs1 = browser.find_element(By.XPATH, "/html/body/form/div/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[4]/td/table/tbody/tr[2]/td/table/tbody/tr/td/div/table/tbody/tr[3]/td/table/tbody/tr[2]/td/table/tbody/tr[4]/td[11]/input").get_attribute("value");
-    except:
-        plannedhrs1 = 'no data' #try to find the first planned hours. If none, will say no data
+    WO = wait.until(EC.element_to_be_clickable((By.ID, 'm52945e17-tb'))).get_attribute('value')
+    print(f'Work Order: {WO}')
+    description = wait.until(EC.element_to_be_clickable((By.ID, 'md42b94ac-tb'))).get_attribute('value')
+    print(f'Description: {description}')
+    jobplan = wait.until(EC.element_to_be_clickable((By.ID, 'mfe7bb84-tb'))).get_attribute('value')
+    print(f'Job Plan: {jobplan}')
+    tstart = wait.until(EC.element_to_be_clickable((By.ID, 'm651c06b0-tb'))).get_attribute('value')
+    print(f'Target Start: {tstart}')
+    asset = wait.until(EC.element_to_be_clickable((By.ID, 'm3b6a207f-tb'))).get_attribute('value') 
+    print(f'Asset: {asset}')
 
-    if plannedhrs1 == 'no data':
+    # Navigate to Plans
+    plans = wait.until(EC.element_to_be_clickable((By.ID, 'm356798d1-tab_anchor')))
+    plans.click()
+    time.sleep(5)
+
+    # There may be multiple laboreres
+    labstring = wait.until(EC.element_to_be_clickable((By.ID, 'm5e4b62f0-lb3')))
+    match = re.search(r'\((\d+) - (\d+) of (\d+)\)', labstring.text)
+    labor_max = int(match.group(3))
+    j = 0; plannedhrs = 0
+
+    while j < labor_max:
         time.sleep(2)
-        try: #try again just to be sure
-            plannedhrs1 = browser.find_element(By.XPATH, "/html/body/form/div/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[4]/td/table/tbody/tr[2]/td/table/tbody/tr/td/div/table/tbody/tr[3]/td/table/tbody/tr[2]/td/table/tbody/tr[4]/td[11]/input").get_attribute("value");
-        except:
-            print('even on a 2nd try plannedhrs1 has no data. Maybe there really is no data')
+        print(f'Index {j + 1} of {labor_max}')
 
-    try:
-        plannedhrs = int(plannedhrs1[0])
-    except:
-        print('no data') #plannedhrs is the sum variable of plannedhrs1, plannedhrs2, and plannedhrs3. It changes depending on work order so I just trial and error it here. If the line above fails it just prints no data.
+        hours_html = f'm5e4b62f0_tdrow_[C:9]_txt-tb[R:{j}]'
+        hrs, mins = map(int, wait.until(EC.element_to_be_clickable((By.ID, hours_html))).get_attribute("value").split(':'))
 
-    try:
-        plannedhrs = int(plannedhrs1[0] + plannedhrs1[1])
-    except:
-        print('no double digits or still no data') 
+        plannedhrs = hrs + mins / 60 + plannedhrs
+        print(f'Planned hours: {plannedhrs}')
+
+        if j % 6 == 5:
+            pej_flipa = wait.until(EC.element_to_be_clickable((By.ID, 'm5e4b62f0-ti7')))
+            pej_flipa.click()
+            time.sleep(3)
         
-    if plannedhrs1 == 'no data':
-        plannedhrs = 'no data'
+        j += 1
         
-#from the above, if no data in plannedhrs1, then plannedhrs is no data. otherwise, it will test with 1
-#character, and then test again with 2.
-
-#plannedhrs 2 section
-        
-    try:
-        plannedhrstwo = browser.find_element(By.ID, "m5e4b62f0_tdrow_[C:9]_txt-tb[R:1]").get_attribute("value");
-    except:
-        plannedhrstwo = 'no data'
-    else:
-        plannedhrs2 = plannedhrstwo[0]
-
-    try:
-        plannedhrs2 = int(plannedhrs2)
-    except:
-        print('no data')
-
-    try:
-        plannedhrs2 = int(plannedhrstwo[0] + plannedhrstwo[1])
-    except:
-        print('less than 10 hours or still no data')
-
-        
-    try:
-        plannedhrs = plannedhrs + plannedhrs2 #if plannedhrs2 doesnt exist, it prints no data for planned hrs 2
-    except:
-        print('no data for plannedhrs2')
-
-
-#plannedhrs 3 section (identical to plannedhrs 2)
-
-    try:
-        plannedhrsthree = browser.find_element(By.ID, "m5e4b62f0_tdrow_[C:9]_txt-tb[R:2]").get_attribute("value");
-    except:
-        plannedhrsthree = 'no data'
-    else:
-        plannedhrs3 = plannedhrsthree[0]
-
-    try:
-        plannedhrs3 = int(plannedhrs3)
-    except:
-        print('no data')
-
-    try:
-        plannedhrs3 = int(plannedhrsthree[0] + plannedhrsthree[1])
-    except:
-        print('less than 10 hours or still no data')
-        
-    try:
-        plannedhrs = plannedhrs + plannedhrs3
-    except:
-        print('no data for plannedhrs3')
-
-#if all of these fail, then from the plannedhrs1 section, plannedhrs will equal 'no data'
-        
-#write to excel and move on to next wo
-    sheet['A' + str(5+i)].value = WO #print WO value to cell A:5+i. i is the number of times the loop has run, which counts the work order number. the 5 added is because of the formatting of the excel sheet.
+    # Excel writing
+    sheet['A' + str(5+i)].value = WO 
     sheet['B' + str(5+i)].value = jobplan
     sheet['C' + str(5+i)].value = description
     sheet['E' + str(5+i)].value = plannedhrs
@@ -226,26 +195,9 @@ while i<numberofWOs:
     sheet['F' + str(5+i)].value = asset
 
     if sheet['E' + str(5+i)].value == 0:
-        sheet['E' + str(5+i)].value = 0.5 #If planned hours = 0 in excel, change to 0.5
+        sheet['E' + str(5+i)].value = 0.5 
 
-        
-    nextElem = browser.find_element(By.ID, "toolactions_NEXT-tbb_image") #the next work order button
-    type(nextElem)
-    time.sleep(20)
-    try:
-        nextElem.click() #click next work order button
-    except:###NEW
-        nextElem.click() #if that didnt work try again
-    wotabElem = browser.find_element(By.ID, "mbf28cd64-tab_anchor") #work order tab button
-    type(wotabElem)
-    time.sleep(5)
-    wotabElem.click() #click the work order tab
-    time.sleep(5)
-    print(WO + ' complete \n') #progress notification
-    plannedhrs2 = 0
-    plannedhrs3 = 0
-    i=i+1
+    print(f'Work Order {WO} completed...')
 
-wb.save() #after the while loop ends, save, close excel, close browser.
-wb.app.quit()
+wb.save() 
 browser.quit()
